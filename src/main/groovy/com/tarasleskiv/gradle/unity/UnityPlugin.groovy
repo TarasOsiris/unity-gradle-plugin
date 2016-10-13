ed2d91a58f8c810ca879be8e089ce20b8f0e3319
Taras Leskiv <taras@getsocial.im>
2016-10-13 14:12:10 +0300

    validation

--- src/main/groovy/com/tarasleskiv/gradle/unity/UnityPlugin.groovy
+++ src/main/groovy/com/tarasleskiv/gradle/unity/UnityPlugin.groovy
@@ -1,10 +1,22 @@
 package com.tarasleskiv.gradle.unity
 
+import org.gradle.api.InvalidUserDataException
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class UnityPlugin implements Plugin<Project> {
     void apply(Project project) {
         project.extensions.create("unity", UnityExtension)
+
+        println(project.unity.unityPath)
+        project.afterEvaluate {
+            if (project.unity.unityPath == null) {
+                throw new InvalidUserDataException("""Path to Unity not set, please specify 'unityPath' in 'unity' plugin config""")
+            }
+
+            if (project.unity.projectPath == null) {
+                throw new InvalidUserDataException("""Path to Unity project not set, please specify 'unityPath' in 'unity' plugin config""")
+            }
+        }
     }
 }
